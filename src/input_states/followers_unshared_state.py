c7330c492d9fb4f3407fc51305e31c3aac2dee85
ThowV <thomasvisscher@outlook.com>
2020-05-24 01:02:39 +0200

    Implement compare progress bar

--- src/input_states/followers_unshared_state.py
+++ src/input_states/followers_unshared_state.py
@@ -1,6 +1,5 @@
-from pprint import pprint
-
 from instagram_private_api import ClientThrottledError
+from rich.progress import Progress
 
 from input_states import menu_state
 from input_states.abstract_input_state import AbstractInputState
@@ -12,18 +11,34 @@ class FollowersUnsharedInputState(AbstractInputState):
 
     def on_enter(self, curr_input_state, client):
         self.console.print("\nLooking for people who don't follow you back.", style="light_salmon3")
-        self.console.print("Comparing following with followers...")
 
         try:
+            # Get both following and followers
             following = client.user_following(client.authenticated_user_id, client.generate_uuid()).get("users")
             followers = client.user_followers(client.authenticated_user_id, client.generate_uuid()).get("users")
 
-            for account in following:
-                for idx, follower in enumerate(followers):
-                    if account.get("pk") == follower.get("pk"):
-                        break  # This account follows us so we stop comparing
-                    if idx == len(followers) - 1:
-                        self.unshared.append(account)  # This account does not follow us back
+            with Progress() as progress:
+                # Progress bar
+                compare_task_length = len(following) * len(followers)
+                compare_task_progress = 0
+                compare_task = progress.add_task("Comparing...", total=compare_task_length)
+
+                for account in following:
+                    for idx, follower in enumerate(followers):
+                        # Update the progress bar
+                        compare_task_progress += 1
+                        progress.update(compare_task, advance=1)
+
+                        # Compare following with follower
+                        if account.get("pk") == follower.get("pk"):
+                            break  # This account follows us so we stop comparing
+                        if idx == len(followers) - 1:
+                            self.unshared.append(account)  # This account does not follow us back
+
+                # Finish the progress bar if the process finished faster than expected
+                if compare_task_progress != compare_task_length:
+                    progress.update(compare_task, advance=compare_task_length - compare_task_progress)
+
         except ClientThrottledError:
             self.console.print("You made too many API requests, try again later.", style="red3")
             return menu_state.MenuInputState(), client
